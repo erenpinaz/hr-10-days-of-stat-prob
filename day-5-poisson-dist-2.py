e1cd321ee1422a906b712b2bc5c19ee6c0d6f243
Eren <eren@pop-os.localdomain>
2020-07-09 22:39:05 +0300

    day-5-2-dist

--- day-5-poisson-dist-2.py
+++ day-5-poisson-dist-2.py
@@ -1,12 +1,15 @@
 import math
 
-def poi_dist(l, k):
-    return (pow(l, k) * pow(2.71828, -1 * l)) / math.factorial(k)
+def poi_exp(a):
+    return (a + pow(a, 2))
 
-def calculate_cost(t, x):
-    return t + (40 * pow(x, 2))
+def calculate_costA(ex):
+    return 160 + (40 * ex)
+
+def calculate_costB(ey):
+    return 128 + (40 * ey)
 
 l = list(map(float, input().split()))
 
-print('{0:.3f}'.format(poi_dist(l[0], calculate_cost(160, l[0]))), end = ' ')
-print('{0:.3f}'.format(poi_dist(l[1], calculate_cost(140, l[1]))), end = ' ')
+print('{0:.3f}'.format(calculate_costA(poi_exp(l[0]))))
+print('{0:.3f}'.format(calculate_costB(poi_exp(l[1]))))
